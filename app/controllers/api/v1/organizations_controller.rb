86db7ebf5b2c614e26c86283287e3cbd6729ccd9
Nadine <nad4ne@gmail.com>
2018-07-29 12:17:25 -0700

    org index

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::OrganizationsController < ApplicationController
+    def index
+      @orgs = Organization.all
+      render json: @orgs, include: :items_neededs
+    end
+
+    def create
+
+    end
+
     def show
     @org = Organization.find_by(id: params[:id])
     render json: @org, include: :items_neededs, status: :ok
--- base64.rb
+++ base64.rb
@@ -1,5 +1,21 @@
-require 'base64'
+# require 'base64'
+# require 'csv'
 
-str = "cXVhbnRpdHkgbmVlZGVkLGl0ZW0gbmVlZGVkDTMwMCxjb2F0cw02LGNhdHMNNyxjb3dzIGEgbWlsa2luDTk4NDcsc3Bvbmdlcw02LGNhbnMgb2YgdHVuYQ04LHN0dWZmcw0yMzVkZixhYWFhYQ1oaWlpaWlpLGFzZGtmamFzbGtk"
+# str = "cXVhbnRpdHlfbmVlZGVkLGl0ZW0NCjMwMCxjb2F0cw0KNixjYXRzDQo3LGNvd3MgYSBtaWxraW4NCjk4NDcsc3Bvbmdlcw0KNixjYW5zIG9mIHR1bmENCjgsc3R1ZmZzDQoyMzVkZixhYWFhYQ0KaGlpaWlpaSxhc2RrZmphc2xrZA0K"
 
-p Base64.decode64(str)
+# decode_str = Base64.decode64(str)
+
+# csv = CSV.parse(decode_str)
+# p csv
+
+arr = [["quantity_needed", "item"], ["300", "coats"], ["6", "cats"], ["7", "cows a milkin"], ["9847", "sponges"], ["6", "cans of tuna"], ["8", "stuffs"], ["235df", "aaaaa"], ["hiiiiii", "asdkfjaslkd"]]
+
+# arr2 = arr.shift
+# p arr[0]
+
+arr2 = []
+arr.each do |a|
+  arr2 << {item: a[1], quantity_needed: a[0]}
+end
+
+p arr2
